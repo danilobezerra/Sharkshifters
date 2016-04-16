e8b78489cd54955fe9d678ea10922540cf95b016
Danilo Bezerra <danilobezerra@outlook.com.br>
2016-04-16 19:39:03 -0300

    MainMenu initial implementation complete

---
+++ Assets/Scripts/MainMenuActions.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuActions : MonoBehaviour {
+	[SerializeField] private string gameSceneName;
+	
+	private bool gameIsReady = false;
+	
+	private void Update() {
+		if (gameIsReady && Input.anyKeyDown) {
+			SceneManager.LoadScene(gameSceneName);
+		}
+	}
+	
+	public void SetGameReady() {
+		if (!gameIsReady) gameIsReady = true;
+	}
+}
