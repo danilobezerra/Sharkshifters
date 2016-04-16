ae9239c5076207826bceb4f193da4e780b2b6e78
Danilo Bezerra <danilobezerra@outlook.com.br>
2016-04-16 13:42:31 -0300

    Attack implementation initiated

---
+++ Assets/Scripts/ProjectileController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class ProjectileController : MonoBehaviour {
+	[SerializeField] private AudioClip hitSound;
+	
+	public delegate void ShootAction(Transform transform);
+	public delegate void HitAction(GameObject projectile);
+	
+	public event ShootAction OnShoot;
+	public event HitAction OnHitOther;
+	
+	private void Update() {
+		if (OnShoot != null) {
+			OnShoot(transform);
+		}
+	}
+	
+	private void OnTriggerEnter2D(Collider2D other) {
+		if (OnHitOther != null) {
+			AudioSource.PlayClipAtPoint(hitSound, transform.position);
+			OnHitOther(other.gameObject);
+			Destroy(gameObject);
+		}
+	}
+	
+	private void OnBecameInvisible() {
+		Destroy(gameObject);
+	}
+ }
--- Assets/Scripts/ShipAttack.cs
+++ Assets/Scripts/ShipAttack.cs
@@ -1,12 +1,39 @@
 ﻿using UnityEngine;
 
-public class ShipAttack : MonoBehaviour {
+public class ShipAttack : MonoBehaviour {	
+	[SerializeField] private GameObject projectilePrefab;
 
-	private void Start() {
+	[SerializeField] private Transform startingPoint;
 	
-	}
+	[SerializeField] private float projectileVelocity;
+	
+	[SerializeField] private AudioClip shootingSound;
 	
 	public void HandleAttack() {
-		Debug.Log("Fire!!");
+		GameObject instance = InstantiateProjetile();
+		
+		var projectile = instance.GetComponent<ProjectileController>();
+		projectile.OnHitOther += HitEnemyAction;
+		projectile.OnShoot += ShootAction;
+	}
+	
+	private GameObject InstantiateProjetile() {
+		Vector2 shotPosition = startingPoint.transform.position;
+		AudioSource.PlayClipAtPoint(shootingSound, shotPosition);
+		
+		Object obj = Instantiate(projectilePrefab, shotPosition, Quaternion.identity);
+		return obj as GameObject;
+	}
+	
+	private void ShootAction(Transform transform) {
+		transform.Translate(Vector2.up * Time.deltaTime * projectileVelocity);
+	}
+	
+	private void HitEnemyAction(GameObject other) {
+		if (other.CompareTag("Enemy")) {
+			Debug.Log("Enemy down!!");
+		} else if (other.CompareTag("NotEnemy")) {
+			Debug.Log("Attacked a teammate!!");
+		}
 	}
 }
